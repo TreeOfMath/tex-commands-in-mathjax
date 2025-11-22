ba114da7e598e4c72b402feea0b72c50d5b48b40
Carol Burns <carolburns50@live.com>
2025-11-22 06:58:24 -0700

    Initial commit

---
+++ auxiliary.js
@@ -0,0 +1,19 @@
+﻿function loadMathJax() { 
+    var text = "";
+    text += "<script type=\"text/x-mathjax-config\">";
+    text += "  MathJax.Hub.Config({";
+    text += "    extensions: [\"TeX/cancel.js\"],";
+    text += "    tex2jax: {";
+    text += "      inlineMath: [ ['$','$'] ],";
+    text += "      displayMath: [ ['$$','$$'] ],";
+    text += "      processEscapes: true";
+    text += "    }";
+    text += "  });";
+    text += "<\/script>";
+    text += "<script async ";
+    text += "src='https://cdn.jsdelivr.net/npm/mathjax@2/MathJax.js?config=TeX-AMS_CHTML'>";
+    text += "<\/script>";
+    return text;
+}
+
+
